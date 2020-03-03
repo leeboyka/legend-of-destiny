3121028cde4f803f4fb51f7c55649cc6b682ac81
leeboyka <13572081952@163.com>
2020-03-03 23:05:51 +0800

    新建单模块【登录】项目

---
+++ login/src/main/java/com/destiny/login/LoginApplication.java
@@ -0,0 +1,19 @@
+package com.destiny.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 登录-程序启动类
+ *
+ * @author leeboyka
+ * @date 2020-03-03 22:55
+ */
+@SpringBootApplication
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+
+}
