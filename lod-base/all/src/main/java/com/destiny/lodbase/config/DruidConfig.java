6d87570edb4926e4a4ded2753aedaabd4b5877c2
leeboyka <13572081952@163.com>
2020-03-22 00:50:09 +0800

    新建lod-base工程

---
+++ lod-base/all/src/main/java/com/destiny/lodbase/config/DruidConfig.java
@@ -0,0 +1,53 @@
+package com.destiny.lodbase.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Druid监控配置类
+ *
+ * @author leeboyka
+ * @date 2020/03/33 00:43
+ */
+@Configuration
+public class DruidConfig {
+
+    /**
+     * 注册 Servlet 组件,监控
+     *
+     * @return ServletRegistrationBean
+     */
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> statViewServlet() {
+        //servletRegistrationBean提供的类进行注册
+        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+        //添加初始化参数initParams
+        //servletRegistrationBean.addInitParameter("allow", "192.168.1.3"); //白名单
+        //servletRegistrationBean.addInitParameter("deny", "192.168.1.3"); //黑名单;与白名单共存时,deny优先于allow
+        //登录查看信息的账号和密码
+        servletRegistrationBean.addInitParameter("loginUsername", "druid");
+        servletRegistrationBean.addInitParameter("loginPassword", "leeboyka");
+        //能否重置数据
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册Filter组件，过滤
+     *
+     * @return FilterRegistrationBean
+     */
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> statFilter() {
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>(new WebStatFilter());
+        //添加过滤规则，验证所有请求
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加需要忽略的信息格式，对 *.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/* 不进行验证
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
---
+++ lod-base/all/src/main/java/com/destiny/lodbase/config/SwaggerConfig.java
@@ -0,0 +1,81 @@
+package com.destiny.lodbase.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * swagger ui config
+ *
+ * @author leeboyka
+ * @date 2020/03/22 00:55
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Value("${swagger.enable}")
+    private boolean enableSwagger;
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Legend Of Destiny")
+                .description("base of LOD API")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
+                .contact(new Contact("lee", "https://weibo.com/5993923898/", "13572081952@163.com"))
+                .build();
+    }
+
+
+    @Bean
+    public Docket createHnsApi() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(enableSwagger)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.destiny.lodbase.common.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .groupName("common");
+    }
+
+
+    /**
+     * swagger ui资源映射
+     *
+     * @param registry ViewControllerRegistry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars*")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    /**
+     * swagger-ui.html路径映射，浏览器中使用/api-docs访问
+     *
+     * @param registry ViewControllerRegistry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api-docs", "/swagger-ui.html");
+    }
+
+}
