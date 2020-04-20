d957482c2c4e18d2d34bebc2584f3fe7d71338a6
leeboyka <13572081952@163.com>
2020-04-20 23:59:44 +0800

    阅读页面左侧工具栏及设置页面样式

--- lod-view/src/views/story/ReadView.vue
+++ lod-view/src/views/story/ReadView.vue
@@ -1,28 +1,145 @@
 <template>
   <el-row class="read-view">
     <div class="note" :style="note">
-      <el-row type="flex" justify="center" :gutter="20">
+      <el-row type="flex" justify="center" :gutter="10">
+        <el-col :span="1" style="min-width: 5em">
+        </el-col>
+        <el-col :span="15">
+          d
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="center" :gutter="10">
         <el-col :span="1" style="min-width: 5em">
           <el-card shadow="always" :style="note2">
-            <div class="toolbar-div">
-              <div><i class="el-icon-notebook-2 toolbar-icon-size"/><div class="toolbar-title">目录</div></div>
-            </div>
-            <div class="toolbar-div">
-              <div><i class="el-icon-s-tools toolbar-icon-size"/><div class="toolbar-title">设置</div></div>
-            </div>
+            <el-popover
+              placement="right-start"
+              width="400"
+              trigger="click">
+              <div>
+                <el-row>目录</el-row>
+                <el-divider></el-divider>
+                <el-row>
+                  <el-table :data="gridData" size="small"  :show-header="false">
+                    <el-table-column property="name">
+                      <template slot-scope="scope">
+                        {{scope.row.date}}{{scope.row.name}}
+                      </template>
+                    </el-table-column>
+                  </el-table>
+                </el-row>
+              </div>
+              <a slot="reference">
+                <div class="toolbar-div">
+                  <div><i class="el-icon-notebook-2 toolbar-icon-size"/><div class="toolbar-title">目录</div></div>
+                </div>
+              </a>
+            </el-popover>
+            <el-popover
+              placement="right"
+              width="400"
+              trigger="click">
+              <div>
+                <el-row>设置</el-row>
+                <el-divider></el-divider>
+                <el-row style="margin-bottom: 10px">
+                  <el-col :span="5" style="text-align:center;height: 40px;line-height: 40px">阅读主题：</el-col>
+                  <el-col :span="19">
+                    <el-row type="flex" justify="center" :gutter="20" style="padding: 0 20px">
+                      <el-col :span="4"><div style="width: 40px;height: 40px;border-radius: 20px;background-color: #42b983"></div></el-col>
+                      <el-col :span="4"><div style="width: 40px;height: 40px;border-radius: 20px;background-color: #332233"></div></el-col>
+                      <el-col :span="4"><div style="width: 40px;height: 40px;border-radius: 20px;background-color: #7562d0"></div></el-col>
+                      <el-col :span="4"><div style="width: 40px;height: 40px;border-radius: 20px;background-color: #c1c1c1"></div></el-col>
+                      <el-col :span="4"><div style="width: 40px;height: 40px;border-radius: 20px;background-color: #c1c1c1"></div></el-col>
+                      <el-col :span="4"><div style="width: 40px;height: 40px;border-radius: 20px;background-color: #c1c1c1"></div></el-col>
+                    </el-row>
+                  </el-col>
+                </el-row>
+                <el-row style="margin-bottom: 10px">
+                  <el-col :span="5" style="text-align: center">正文字体：</el-col>
+                  <el-col :span="19">
+                    <el-row type="flex" justify="left" :gutter="20" style="padding: 0 20px">
+                      <el-col :span="6"><el-button size="mini">楷体</el-button></el-col>
+                      <el-col :span="6"><el-button size="mini">黑体</el-button></el-col>
+                      <el-col :span="6"><el-button size="mini">宋体</el-button></el-col>
+                      <el-col :span="6"><el-button size="mini">雅黑</el-button></el-col>
+                    </el-row>
+                  </el-col>
+                </el-row>
+                <el-row style="margin-bottom: 10px">
+                  <el-col :span="5" style="text-align: center">字体大小：</el-col>
+                  <el-col :span="19">
+                    <el-row type="flex" justify="left" :gutter="20" style="padding: 0 20px">
+                      <el-col :span="8"><el-button size="mini">小号字体</el-button></el-col>
+                      <el-col :span="8"><el-button size="mini">正常字体</el-button></el-col>
+                      <el-col :span="8"><el-button size="mini">大号字体</el-button></el-col>
+                    </el-row>
+                  </el-col>
+                </el-row>
+                <el-row style="margin-bottom: 10px">
+                  <el-col :span="5" style="text-align: center">页面宽度：</el-col>
+                  <el-col :span="19">
+                    <el-row type="flex" justify="left" :gutter="20" style="padding: 0 20px">
+                      <el-col :span="8"><el-button size="mini">较窄页面</el-button></el-col>
+                      <el-col :span="8"><el-button size="mini">正常页面</el-button></el-col>
+                      <el-col :span="8"><el-button size="mini">较宽页面</el-button></el-col>
+                    </el-row>
+                  </el-col>
+                </el-row>
+                <el-row type="flex" justify="center" :gutter="20" style="margin-top: 15px">
+                  <el-button size="mini" type="primary" round>保存设置</el-button>
+                  <el-button size="mini" type="warning" round>恢复默认</el-button>
+                </el-row>
+              </div>
+              <a slot="reference">
+                <div class="toolbar-div">
+                  <div><i class="el-icon-s-tools toolbar-icon-size"/><div class="toolbar-title">设置</div></div>
+                </div>
+              </a>
+            </el-popover>
             <div class="toolbar-div">
               <div><i class="el-icon-star-on toolbar-icon-size"/><div class="toolbar-title">收藏</div></div>
             </div>
             <div class="toolbar-div">
               <div><i class="el-icon-s-flag toolbar-icon-size"/><div class="toolbar-title">投票</div></div>
             </div>
-            <div class="toolbar-div">
-              <div><i class="el-icon-coin toolbar-icon-size"/><div class="toolbar-title">打赏</div></div>
-            </div>
+            <el-popover
+              placement="right"
+              width="400"
+              trigger="click">
+              <div>
+                <el-row>打赏</el-row>
+                <el-divider></el-divider>
+                <el-row>
+                  <el-col :span="3" :offset="1">宝石：</el-col>
+                  <el-col :span="8">
+                    <el-rate
+                      v-model="value2"
+                      :colors="colors"
+                      :max="3"
+                    >
+                    </el-rate>
+                  </el-col>
+                  <el-col :span="3" :offset="1">金币：</el-col>
+                  <el-col :span="8">
+                    <el-rate
+                      v-model="value2"
+                      :colors="colors"
+                    >
+                    </el-rate>
+                  </el-col>
+                </el-row>
+              </div>
+              <a slot="reference">
+                <div class="toolbar-div">
+                  <div><i class="el-icon-coin toolbar-icon-size"/><div class="toolbar-title">打赏</div></div>
+                </div>
+              </a>
+            </el-popover>
           </el-card>
         </el-col>
         <el-col :span="15">
           <el-card shadow="always" :style="note1">
+            <input type="file" @change="loadTextFromFile">
             <div class="article">
               <h2>第一章 风月缘</h2>
               <h5>
@@ -32,6 +149,7 @@
                 <span class="article-tag-left"><i class="el-icon-time article-tag"/>&nbsp;2020-01-20 19:00</span>
               </h5>
               <el-divider></el-divider>
+              <span type="text" v-html="story"></span>
               <p>晨初，天生丽质的南宫轻舞于惊艳于镜中自己的身体，不觉自慰。</p>
               <p>又在偷看了哥哥南宫玦与继母赵小婵客厅椅上私会，后被哥哥诱骗到花园亭中意乱暧昧，在最后一刻醒悟逃离。</p>
               <p>父南宫易天回后带一盲僧，在仔细检验过裸身的轻舞后，断定其有仙体狐阴。南宫易天便将其送与族长之子北冥惊雷为妻以示讨好，南宫轻舞不愿，离家出逃。</p>
@@ -49,14 +167,15 @@
               <p>又是每月例行考教之日，东郭幽兰谴燕儿来召云逸尘，途经无人巷时，云逸尘以告知幽兰他猥亵燕儿一事相威胁，得以手探燕儿前阴后庭，终使燕儿高潮。</p>
               <p>在幽兰处，幽兰教二人切磋学习。燕儿忆起几番受辱，徒下杀手却被云逸尘击晕。云逸尘祭出血刀架于燕儿颈上逼幽兰口交，幽兰解开衣襟袒露椒乳，双膝跪地挺起翘臀，为他吸卵吹箫，临近高潮之时幽兰正欲吐出，云逸尘猛的抱住她脑袋，挺进咽喉一泄如注，让幽兰喝下了浓精。</p>
               <p>幽兰欲趁云松懈时反扑，却因乳头突遭咬袭，羞痛之下顿时散功反被血刀火劲入体。云趁机种血蛊逼其保守因血刀导致身份败露一事。幽兰只得屈从，更被令将昏死的燕儿扒个精光，翘臀猫腰趴在身上舔舐揉弄其脖颈乳房。云逸尘则在身后扯着秀发于肛门屁眼里泄了个尽兴。</p>
-              <p></p>
-              <p></p>
-              <p></p>
-              <p></p>
-              <p></p>
-              <p></p>
             </div>
           </el-card>
+        </el-col>
+      </el-row>
+      <el-row type="flex" justify="center" :gutter="10">
+        <el-col :span="1" style="min-width: 5em">
+        </el-col>
+        <el-col :span="15">
+          d
         </el-col>
       </el-row>
     </div>
@@ -68,6 +187,26 @@ export default {
   name: 'ReadView',
   data () {
     return {
+      gridData: [{
+        date: '第一章：',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '第二章：',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '第三章：',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '第四章：',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
+      story: '',
+      value2: null,
+      colors: ['#99A9BF', '#F7BA2A', '#FF9900'],
       note: {
         backgroundImage: 'url(' + require('../../assets/background.jpg') + ')',
         backgroundRepeat: 'repeat',
@@ -84,6 +223,28 @@ export default {
         backgroundRepeat: 'repeat',
         backgroundSize: '25px auto'
       }
+    }
+  },
+
+  methods: {
+    loadTextFromFile: function (e) {
+      const files = e.target.files
+      if (files.length > 0) {
+        const file = files[0]
+        // 读取文件名
+        const name = file.name
+        // 读取文件大小
+        const size = file.size
+        console.log(name, size)
+        const reader = new FileReader()
+        reader.onload = e => this.$emit('load', this.dealNum(e.target.result))
+        reader.readAsText(file, 'gb2312')
+      }
+    },
+    dealNum (item) {
+      let word = '<p>' + item.replace(/\n|\r\n/g, '</p>')
+      this.story = word
+      console.log(word)
     }
   }
 }
